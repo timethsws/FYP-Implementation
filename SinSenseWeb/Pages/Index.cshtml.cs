25641b01b2a8f6e6e12b7e3bd8ae48751cc15a8a
Timeth Subasinghe <thimet.subasinghe@itelasoft.com>
2020-05-04 20:03:20 +0530

    changes

--- SinSenseWeb/Pages/Index.cshtml.cs
+++ SinSenseWeb/Pages/Index.cshtml.cs
@@ -13,36 +13,15 @@ namespace SinSense.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly GoogleTranslateService _translaterService;
-
-        [BindProperty]
-        public String text { get; set; }
-
-        [BindProperty]
-        public string translated { get; set; }
+       
         public IndexModel(ILogger<IndexModel> logger,GoogleTranslateService translaterService)
         {
-            _logger = logger;
-            _translaterService = translaterService;
+          
         }
 
         public void OnGet()
         {
-            text = DateTime.Now.ToLongDateString();
+            
         }
-        public IActionResult OnPost()
-        {
-            //var translate = Request.Form["translate"];
-            //if (!string.IsNullOrWhiteSpace(translate))
-            //{
-            //    translated = _translaterService.Translate(translate);
-            //}
-            //text = $"Translated \"{translate}\"";
-            translated = "turned off for now :(";
-            text = $"We'll be back soon :)";
-            return Page();
-        }
-        
     }
 }
